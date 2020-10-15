ee92067bfb43339e48422cd0685aef8ecf94ce98
Konstantinos <kongallis@gmail.com>
2020-10-15 14:37:02 +0300

    Solution to problem set 1 - Mario version 2

---
+++ mario_2/mario2.c
@@ -0,0 +1,57 @@
+#include <stdio.h>
+#include <string.h>
+
+int main()
+{
+    int numberOfLines;
+    printf("Enter a number between 1 and 8: ");
+    scanf("%d", &numberOfLines);
+    printf("Number of lines = %d \n", numberOfLines);
+
+    if (numberOfLines >= 1 || numberOfLines <= 8)
+    {
+
+        if (numberOfLines == 1)
+        {
+            printf("#  #\n");
+        }
+        else
+        {
+            for (int i = 1; i <= numberOfLines; i++)
+            {
+                int numberOfSpaces = numberOfLines - i;
+                int numberOfBricks = i;
+
+                // Twice the size of spaces and bricks plus 2 spaces between
+                char line[2 * numberOfLines + 2];
+                for (int x = 1; x <= numberOfSpaces; x++)
+                {
+                    strcat(line, " ");
+                }
+                for (int y = 1; y <= numberOfBricks; y++)
+                {
+                    strcat(line, "#");
+                }
+                // Add spaces between
+                strcat(line, "  ");
+                for (int y = 1; y <= numberOfBricks; y++)
+                {
+                    strcat(line, "#");
+                }
+                for (int x = 1; x <= numberOfSpaces; x++)
+                {
+                    strcat(line, " ");
+                }
+
+                printf("result: %s\n", line);
+            }
+        }
+    }
+    else
+    {
+        printf("Incorrect input.");
+        main();
+    }
+
+    return 0;
+}
