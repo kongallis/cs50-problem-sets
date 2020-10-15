6f1d31b4e5f5438eb01d68f656e3785c97a58264
Konstantinos <kongallis@gmail.com>
2020-10-15 14:20:18 +0300

    Solution to problem set 1 - Mario version 1

--- mario_1/mario1.c
+++ mario_1/mario1.c
@@ -1,9 +1,39 @@
 #include <stdio.h>
+#include <string.h>
 
-int main(void)
+int main()
 {
-    int pyramidHeight;
-    printf("Enter an integer: ");
-    scanf("%d", &pyramidHeight);
-    printf("Number = %d \n", pyramidHeight);
+    int numberOfLines;
+    printf("Enter the number of lines: ");
+    scanf("%d", &numberOfLines);
+    printf("Number of lines = %d \n", numberOfLines);
+
+    if (numberOfLines == 1)
+    {
+        printf("#\n");
+    }
+    else
+    {
+        for (int i = 1; i <= numberOfLines; i++)
+        {
+            int numberOfSpaces = numberOfLines - i;
+            int numberOfBricks = i;
+            // printf("i: %d\n", i);
+            // printf("numberOfSpaces: %d\n", numberOfSpaces);
+            // printf("numberOfBricks: %d\n", numberOfBricks);
+
+            char line[numberOfLines];
+            for (int x = 1; x <= numberOfSpaces; x++)
+            {
+                strcat(line, " ");
+            }
+            for (int y = 1; y <= numberOfBricks; y++)
+            {
+                strcat(line, "#");
+            }
+            printf("result: %s\n", line);
+        }
+    }
+
+    return 0;
 }
