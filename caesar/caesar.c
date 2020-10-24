547d19d50cfb30ecc2b0419fb64c371db6a45330
Konstantinos Gallis <kongallis@gmail.com>
2020-10-24 13:45:47 +0000

    Added command line restrictions. 1 Numerical arg

--- caesar/caesar.c
+++ caesar/caesar.c
@@ -1,8 +1,35 @@
 #include <stdio.h>
 #include <cs50.h>
+#include <string.h>
 
-int main(void)
-{
-    //Reads the plain text from user and prints it out
-    string text = get_string("plaintext: ");
+int main(int argc, char *argv[]) {
+
+    printf("Argument number %d\n", argc);
+    if (argc == 1 || argc > 2) {
+        printf("ERROR: Please insert only 1 argument.\n");
+        return 1;
+    }
+
+    for (int i = 0; i < strlen(argv[1]); i++)
+    {
+        if(!(argv[1][i] >= 48 & argv[1][i] <= 57 ))
+        {
+            printf("Usage: ./caesar key\n");
+            return 1;
+        }
+    }
+
+    int cipherKey =
+    // 26 letters
+    if (argv[1] > 26) {
+
+    }
+    // k = cipher key
+
+    // Without command line args OR with more than 1 command arg
+
+
+
+
+    return 0;
 }
