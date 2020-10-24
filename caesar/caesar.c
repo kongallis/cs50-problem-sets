f795e9bf1ed6a1bab337533a111371588bd77fc8
Konstantinos <kongallis@gmail.com>
2020-10-24 19:50:52 +0300

    Style according to Harvard manual and added comments

--- caesar/caesar.c
+++ caesar/caesar.c
@@ -3,19 +3,23 @@
 #include <string.h>
 #include <stdlib.h>
 
-string calcCipherText(string text,int cipherKey);
+string calcCipherText(string text, int cipherKey);
 
-int main(int argc, char *argv[]) {
-
+// Pass the command promt argumens
+int main(int argc, char *argv[])
+{
+    // Checks if there is only 1 argument
     printf("Argument number %d\n", argc);
-    if (argc == 1 || argc > 2) {
+    if (argc == 1 || argc > 2)
+    {
         printf("ERROR: Please insert only 1 argument.\n");
         return 1;
     }
 
+    // Checks if the argument is a number
     for (int i = 0; i < strlen(argv[1]); i++)
     {
-        if(!(argv[1][i] >= 48 & argv[1][i] <= 57 ))
+        if (!(argv[1][i] >= 48 & argv[1][i] <= 57))
         {
             printf("Usage: ./caesar key\n");
             return 1;
@@ -24,59 +28,45 @@ int main(int argc, char *argv[]) {
 
     int cipherKey = atoi(argv[1]) % 26;
 
-    // printf("Cipher Key = %d\n", cipherKey);
-
-   string text = get_string("plaintext: ");
-   string cipherText = calcCipherText(text,cipherKey);
-   printf("ciphertext: %s\n", cipherText);
-
-
-
-
-
-
-
+    // Get the plain text from the user
+    string text = get_string("plaintext: ");
+    string cipherText = calcCipherText(text, cipherKey);
+    printf("ciphertext: %s\n", cipherText);
     return 0;
 }
 
+// Translates each letter of the plaintext to ciphertext
+string calcCipherText(string text, int cipherKey)
+{
 
-string calcCipherText(string text, int cipherKey) {
-
-    // char plainText[strlen(text)] = text;
-    for (int i = 0; i < strlen(text); i++){
+    for (int i = 0; i < strlen(text); i++)
+    {
         char character = text[i];
-        // lowercase
-       if ( character >= 97 && character <= 122){
-             /// character + cipherkey => 130
-             // orio einai 122 prepei => 97 + 8
+        // lowercase letters
+        if (character >= 97 && character <= 122)
+        {
+            int result = character + cipherKey;
+            result = result % 122;
 
-             int result = character + cipherKey;
-             result = result % 122;
+            if (result < 97)
+            {
+                result += 96;
+            }
+            text[i] = result;
+        }
+        // uppercase letters
+        else if (character >= 65 && character <= 90)
+        {
+            int result = character + cipherKey;
+            result = result % 90;
 
-             if (result < 97)
-             {
-                 result += 96;
-             }
-             text[i] = result;
-       }
-       // uppercase
-       else if ( character >= 65 && character <= 90  ){
-             int result = character + cipherKey;
-             result = result % 90;
-
-             if (result < 65)
-             {
-                 result += 64;
-             }
-             text[i] = result;
-       }
+            if (result < 65)
+            {
+                result += 64;
+            }
+            text[i] = result;
+        }
     }
-    printf("%s",text);
+    printf("%s", text);
     return text;
 }
-
-
-// gramma --> noumero + cipherKey >
-
-// 65-90
-// 97 - 122
